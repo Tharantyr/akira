884c8a77d44fee59364a7074ba982f548368b123
khchung00 <khchung00@602a7b48-f520-0410-8bd5-356264b4cf1a>
2006-10-29 16:48:21 +0000

    adding more code to Transfrom.cs

--- trunk/math/Transform.cs
+++ trunk/math/Transform.cs
@@ -8,6 +8,50 @@ namespace Akira.Maths
     {
     }
 
+    static public Transform operator *(Transform m1, Transform m2)
+    {
+    }
+
+
+    static public operator *(Transfrom m, Vector3 v)
+    {
+    }
+
+    static public operator *(Transform m, Normal n)
+    {
+    }
+
+    static public operator *(Transfrom m, Point3 p)
+    {
+    }
+
+
+    public Matrix Mat
+    {
+        get
+        {
+            return _mat;
+        }
+        set
+        {
+            _mat = value;
+            _invMat = Matrix.inverse(_mat);
+        }
+    }
+
+
+    public Matrix Inv
+    {
+        get
+        {
+            return _invMat;
+        }
+        set
+        {
+            _invMat = value;
+            _mat = Matrix.inverse(_invMat);
+        }
+    }
 
     Matrix _mat;
     Matrix _invMat;
